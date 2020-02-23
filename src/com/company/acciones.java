f7a379cf9c91bdb3faca6d88f434f35a35bce550
marisaamv <mon19169@uvg.edu.gt>
2020-02-23 11:26:24 -0600

    Cambios en aritmetica, algo sigue en duda talk later

--- src/com/company/acciones.java
+++ src/com/company/acciones.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.ArrayList;
 
 public class acciones {
     public acciones(){
@@ -10,10 +11,121 @@ public class acciones {
     public String defun(){
         return "";
     }
+	public int aritmetica(String[] datos){
+		ArrayList<Integer> numeros = new ArrayList<Integer>(); //Aqui se iran guardando los numeros del arrayList
+		boolean esNumero = false; //Cambia cuando se encuentra con un numero en el array
+		boolean error = false; //Nos indica cuando ya no hay nada mas que recorrer en el array o hay un error en la escritura del código o en el código ingresado
+		int contador = 0; //Nos indica cual de los elementos del aaray estamos leyendo
+		while(contador< datos.lenght && !error){ //mientras que el contador sea menor que el tamaño del array y el error sea falso
+			if(!esNumero){//si es numero es falso
+				if(datos[contador].equals("+")){
+					contador++;
+					while(esNumero= true){
+						try{
+							numeros.add(Integer.parseInt(datos[contador])); //Que meta en numero en nuestro Arraylist solo de numeros
+							esNumero = true;//escribir que es numero es true
+							contador++;
+						}
+						catch(Exception ex){
+							esNumero = false;//Si no es numero se sale del while :)
+						}
+					}
+					while(numeros.size()!=1){//Minettras que el tamaño del array no sea uno
+						int numero1 = numeros.get(0); //Se guarda el numero de la posicion 0 en una variable
+						int numero2 = numeros.get(1);//se guarda el numero de la posicion 1 en una variable
+						numeros.remove(0);//se remueve la posicion 0
+						numeros.remove(0);//se remueve la posicon 0
+						numeros.add(operacionesAritmeticas(1,numero1,numero2);//se añade el resultado al array luego de sumar los dos numeros
+					}
+					return numeros.get(0);//se devuelve el resultado de la suma
+				}
+				else{
+					if(datos[contador].equals("-")){
+						contador++;
+						while(esNumero= true){
+							try{
+								numeros.add(Integer.parseInt(datos[contador])); //Que meta en numero en nuestro Arraylist solo de numeros
+								esNumero = true;//escribir que es numero es true
+								contador++;
+							}
+							catch(Exception ex){
+								esNumero = false;//Si no es numero se sale del while :)
+							}
+						}
+						while(numeros.size()!=1){
+							int numero1 = numeros.get(0);
+							int numero2 = numeros.get(1);
+							numeros.remove(0);
+							numeros.remove(0);
+							numeros.add(operacionesAritmeticas(2,numero1,numero2);
+						}
+						return numeros.get(0);
+					}
+					else{
+						if(datos[contador].equals("*")){
+							contador++;
+							while(esNumero= true){
+								try{
+									numeros.add(Integer.parseInt(datos[contador])); //Que meta en numero en nuestro Arraylist solo de numeros
+									esNumero = true;//escribir que es numero es true
+									contador++;
+								}
+								catch(Exception ex){
+									esNumero = false;//Si no es numero se sale del while :)
+								}
+							}
+							while(numeros.size()!=1){
+								int numero1 = numeros.get(0);
+								int numero2 = numeros.get(1);
+								numeros.remove(0);
+								numeros.remove(0);
+								numeros.add(operacionesAritmeticas(3,numero1,numero2);
+							}
+							return numeros.get(0);
+						}
+						else{
+							if(datos[contador].equals("/")){
+								contador++;
+								while(esNumero= true){
+									try{
+										numeros.add(Integer.parseInt(datos[contador])); //Que meta en numero en nuestro Arraylist solo de numeros
+										esNumero = true;//escribir que es numero es true
+										contador++;
+									}
+									catch(Exception ex){
+										esNumero = false;//Si no es numero se sale del while :)
+									}
+								}
+								while(numeros.size()!=1){
+									int numero1 = numeros.get(0);
+									int numero2 = numeros.get(1);
+									numeros.remove(0);
+									numeros.remove(0);
+									numeros.add(operacionesAritmeticas(4,numero1,numero2);
+								}
+								return numeros.get(0);
+							}
+							else{
+								error=true;
+								return 0;
+								System.out.println("No se pudo realizar la operacion, revisa tus operandos ingresados, solo puedes ingresar - , + , * , /");
+							}
+						}
+					}
+				}
+			}
+			else{
+				error=true;
+				System.out.println("No se puede comenzar con un numero, LISP funciona con un sistema PRE fix");
+				return 0;
+			}
+		}
+	}
+	
 	//Pense en que aqui podiamos aceptar tres parametros, uno en donde decidde si sumar, restar bla bla y los dos cosos que quiere sumar y restar
     //Pre: se ingresan 3 numeros, el que decide la operacion 1= suma, 2=resta, 3=multiplicacion, 4= ivisión
 	//Post: se devuelve el resultado de cualquiera de la operación
-	public int aritmetica(int operacion, int a, int b){
+	public int operacionesAritmeticas(int operacion, int a, int b){
         if(operacion==1){
 			return a+b;
 		}
